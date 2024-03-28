176e3f812a24632c3da74035fc91e8ae74fe90da
Vinicius Rimes de Oliveira <viniciusrimess@gmail.com>
2024-03-27 21:53:56 -0300

    Create ApiExceptionMiddlewareExtensions.cs

---
+++ APICatalog/Extensions/ApiExceptionMiddlewareExtensions.cs
@@ -0,0 +1,33 @@
+﻿using APICatalog.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Net;
+
+namespace APICatalog.Extensions
+{
+    static public class ApiExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message,
+                            Trace = contextFeature.Error.StackTrace
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
