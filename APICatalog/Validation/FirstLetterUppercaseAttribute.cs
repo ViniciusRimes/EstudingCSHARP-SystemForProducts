c43b07670954786727f75883003cacae55ca88de
Vinicius Rimes de Oliveira <viniciusrimess@gmail.com>
2024-03-27 21:56:19 -0300

    Filters de loggers

---
+++ APICatalog/Validation/FirstLetterUppercaseAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalog.Validation
+{
+    public class FirstLetterUppercaseAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, 
+        ValidationContext validationContext)
+        {
+            if(value == null || string.IsNullOrEmpty(value.ToString())){
+                return ValidationResult.Success;
+            }
+            var firstLetter = value.ToString()[0].ToString();
+            if (firstLetter != firstLetter.ToUpper())
+            {
+                return new ValidationResult("A primeira letra do nome do produto deve ser maíuscula!");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
