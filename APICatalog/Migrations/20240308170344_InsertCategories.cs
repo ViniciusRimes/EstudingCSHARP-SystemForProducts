dcd86ef3a4e5a6a7024756c1c598bac60631ad9e
Vinicius Rimes de Oliveira <viniciusrimess@gmail.com>
2024-03-27 21:49:57 -0300

    Insering DBContext and Migrations

---
+++ APICatalog/Migrations/20240308170344_InsertCategories.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into categories(Name, ImageURL) Values('Bebidas', 'bebidas.jpg')");
+            migrationBuilder.Sql("Insert into categories(Name, ImageURL) Values('Alimentos', 'alimentos.jpg')");
+            migrationBuilder.Sql("Insert into categories(Name, ImageURL) Values('Carnes', 'carnes.jpg')");
+            migrationBuilder.Sql("Insert into categories(Name, ImageURL) Values('Eletrônicos', 'eletronicos.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categories");
+        }
+    }
+}
---
+++ APICatalog/Migrations/20240308171158_InsertProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Products(Name, Description, Price, ImageURL, Stock, RegistrationDate, CategoryId) " +
+                "Values('Coca-Cola 2.25L', 'Refrigerante Coca-Cola 2.25L', '9.99', 'bebidas.jpg', 100, now(), 1)");
+            migrationBuilder.Sql("Insert into Products(Name, Description, Price, ImageURL, Stock, RegistrationDate, CategoryId) " +
+                "Values('Arroz Tio João 5kg', 'Arroz Tio João 5kg', '20.99', 'alimentos.jpg', 100, now(), 2)");
+            migrationBuilder.Sql("Insert into Products(Name, Description, Price, ImageURL, Stock, RegistrationDate, CategoryId) " +
+                "Values('Peito de frango 1kg', 'Peito de frango 1kg', '14.99', 'carnes.jpg', 100, now(), 3)");
+            migrationBuilder.Sql("Insert into Products(Name, Description, Price, ImageURL, Stock, RegistrationDate, CategoryId) " +
+                "Values('Peito de frango 1kg', 'Peito de frango 1kg', '14.99', 'carnes.jpg', 100, now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categories");
+        }
+    }
+}
