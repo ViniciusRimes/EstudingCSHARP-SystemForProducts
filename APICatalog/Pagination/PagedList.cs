2dae66a472ae3844a8254c0c69faee6d5f6d6e21
Vinicius Rimes de Oliveira <viniciusrimess@gmail.com>
2024-03-27 21:55:43 -0300

    Paginations of products and categories for consult better

---
+++ APICatalog/Pagination/PagedList.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace APICatalog.Pagination
+{
+    public class PagedList<T> : List<T> where T : class
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public bool hasPrevious => CurrentPage > 1;
+        public bool hasNext => CurrentPage < TotalPages;
+
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            CurrentPage = pageNumber;
+            PageSize = pageSize;
+            TotalPages = (int) Math.Ceiling(count / (double)pageSize);
+            AddRange(items);
+        }
+        public static PagedList<T> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
---
+++ APICatalog/Pagination/ProductsFilteringForPrice.cs
@@ -0,0 +1,10 @@
+﻿namespace APICatalog.Pagination
+{
+    public class ProductsFilteringForPrice : QueryStringParameters
+    {
+        public decimal? Price { get; set; }
+        public string? PriceCritery { get; set; } //maior, menor ou igual
+
+        
+    }
+}
