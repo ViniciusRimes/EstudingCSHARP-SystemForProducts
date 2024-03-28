4f8fc2d1422aba7e5f6c9aef8ffb9a0e77905edf
Vinicius Rimes de Oliveira <viniciusrimess@gmail.com>
2024-03-27 21:52:41 -0300

    Create an initials controllers

---
+++ APICatalog/Controllers/CategoryController.cs
@@ -0,0 +1,120 @@
+﻿using APICatalog.Context;
+using APICatalog.DTOs;
+using APICatalog.DTOs.Mappings;
+using APICatalog.Filters;
+using APICatalog.Models;
+using APICatalog.Pagination;
+using APICatalog.Repositories;
+using APICatalog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NuGet.Protocol.Core.Types;
+
+
+namespace APICatalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IUnityOfWork _unityOfWork;
+
+        private readonly ILogger<CategoryController> _logger;
+
+        public CategoryController(ILogger<CategoryController> logger, ICategoryRepository repository, IUnityOfWork unityOfWork)
+        {
+            _logger = logger;
+            _unityOfWork = unityOfWork;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<CategoryDTO>> GetCategories()
+        {
+            var categories = _unityOfWork.CategoryRepository.GetAll();
+            if(categories == null)
+            {
+                return NotFound("Nenhuma categoria cadastrada!");
+            }
+ 
+            var categoriesDTO = categories.ToCategoryDTOList();
+
+            return Ok(categoriesDTO);   
+        }
+        [HttpGet("{id:int}", Name = "GetCategory")]
+        public ActionResult<CategoryDTO> GetCategoryById(int id)
+        {
+            var category = _unityOfWork.CategoryRepository.Get(c => c.CategoryId == id);
+            if (category is null)
+            {
+                _logger.LogWarning($"Categoria com id {id} não encontrada!");
+                return NotFound("Categoria não encontrada");
+            }
+            var categoryDTO = category.ToCategoryDTO();
+            return Ok(categoryDTO);
+        }
+        [HttpGet("pagination")]
+        public ActionResult<IEnumerable<CategoryDTO>> Get([FromQuery] CategoriesParameters parameters)
+        {
+            var categories = _unityOfWork.CategoryRepository.GetCategories(parameters);
+
+            var metadata = new
+            {
+                categories.PageSize,
+                categories.TotalCount,
+                categories.CurrentPage,
+                categories.TotalPages,
+                categories.hasNext,
+                categories.hasPrevious
+            };
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
+            var categoriesDTO = categories.ToCategoryDTOList();
+            return Ok(categoriesDTO);
+        }
+
+        [HttpPost]
+        public ActionResult<CategoryDTO> CreateCategory(CategoryDTO categoryDTO)
+        {
+            if(categoryDTO is null) {
+                _logger.LogWarning("Dados inválidos");
+                return BadRequest("Dados inválidos");
+            }
+            var category = categoryDTO.ToCategory();
+
+            _unityOfWork.CategoryRepository.Create(category);
+            _unityOfWork.Commit();
+            var newCategoryDTO = category.ToCategoryDTO();
+
+            return new CreatedAtRouteResult("GetCategory", new { id = newCategoryDTO.CategoryId }, newCategoryDTO);
+        }
+        [HttpPut("{id:int}")]
+        public ActionResult<CategoryDTO> EditCategory(int id, CategoryDTO categoryDTO)
+        {
+            if(id != categoryDTO.CategoryId)
+            {
+                _logger.LogWarning("Dados inválidos");
+                return BadRequest("Dados inválidos");
+            }
+            var category = categoryDTO.ToCategory();
+            _unityOfWork.CategoryRepository.Update(category);
+            _unityOfWork.Commit();
+            var newCategoryDTO = category.ToCategoryDTO();
+            return Ok($"Categoria editada\nCategory: {newCategoryDTO}");
+        }
+        [HttpDelete("{id:int}")]
+        public ActionResult<CategoryDTO> DeleteCategory(int id)
+        {
+            var category = _unityOfWork.CategoryRepository.Get(c=> c.CategoryId == id);
+            if(category is null) {
+                _logger.LogWarning("Categoria não encontrada");
+                return NotFound("Categoria não encontrada");
+            }
+            var categoryExcluded = _unityOfWork.CategoryRepository.Delete(category);
+            _unityOfWork.Commit();  
+            var categoryDTO = categoryExcluded.ToCategoryDTO();
+            _logger.LogInformation($"Categoria excluida\nCategoria: {categoryDTO}");
+            return Ok($"Categoria excluída\nCategory: {categoryDTO}");     
+        }
+    }
+}
---
+++ APICatalog/Controllers/ProductController.cs
@@ -0,0 +1,165 @@
+﻿using APICatalog.Context;
+using APICatalog.DTOs;
+using APICatalog.Models;
+using APICatalog.Pagination;
+using APICatalog.Repositories;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace APICatalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnityOfWork _unityOfWork;
+        private readonly ILogger<ProductController> _logger;
+        private readonly IMapper _mapper;
+
+        public ProductController(ILogger<ProductController> logger, IProductRepository productRepository, IUnityOfWork unityOfWork, IMapper mapper)
+        {
+            _logger = logger;
+            _unityOfWork = unityOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductDTO>> GetProducts() 
+        {
+            var products = _unityOfWork.ProductRepository.GetAll();
+            if (products is null)
+            {
+                _logger.LogWarning("Produtos não encontrados");
+                return NotFound("Produtos não encontrados"); 
+            }
+            var productsDTO = _mapper.Map<IEnumerable<ProductDTO>>(products);
+            return Ok(productsDTO);
+        }
+        [HttpGet("{id:int:min(1)}", Name="GetProductById")]
+        public ActionResult<ProductDTO> GetProductById([FromQuery]int id)
+        {
+            var product = _unityOfWork.ProductRepository.Get(p=> p.ProductId == id);
+            if (product is null)
+            {
+                _logger.LogWarning("Produto não encontrado");
+                return NotFound("Produto não encontrado");
+            }
+            var productDTO = _mapper.Map<ProductDTO>(product);
+            return Ok(productDTO);
+        }
+        [HttpGet("products/{id:int}")]
+        public ActionResult <IEnumerable<ProductDTO>> GetProductsByCategory(int id)
+        {
+            var products = _unityOfWork.ProductRepository.GetProductsByCategory(id);
+            if (products is null)
+            {
+                _logger.LogWarning($"Nenhum produto da categoria {id} foi encontrado");
+                return NotFound($"Nenhum produto da categoria {id} foi encontrado");
+            }
+            var productsDTO = _mapper.Map<IEnumerable<ProductDTO>>(products);
+            return Ok(productsDTO);
+        }
+        [HttpGet("pagination")]
+        public ActionResult <IEnumerable<ProductDTO>> Get([FromQuery] ProductsParameters parameters)
+        {
+            var products = _unityOfWork.ProductRepository.GetProducts(parameters);
+
+            object metadata = GetMetadata(products);
+            Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(metadata)); 
+            var productsDTO = _mapper.Map<IEnumerable<ProductDTO>>(products);
+            return Ok(productsDTO);
+        }
+        [HttpGet("filtering/price/pagination")]
+        public ActionResult<IEnumerable<ProductDTO>> GetProductsFilteringForPrice([FromQuery] ProductsFilteringForPrice filters)
+        {
+            var products = _unityOfWork.ProductRepository.GetProductsFilteringForPrice(filters);
+
+            object metadata = GetMetadata(products);
+            Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(metadata));
+            var productsDTO = _mapper.Map<IEnumerable<ProductDTO>>(products);
+            return Ok(productsDTO);
+        }
+
+        [HttpPost]
+        public ActionResult<ProductDTO> CreateProduct(ProductDTO productDTO)
+        {
+            if (productDTO is null)
+            {
+                _logger.LogWarning("Dados inválidos");
+                return BadRequest("Dados inválidos");
+            }
+            var product = _mapper.Map<Product>(productDTO);
+            _unityOfWork.ProductRepository.Create(product);
+            _unityOfWork.Commit();
+            var newProductDTO = _mapper.Map<ProductDTO>(product);
+            return new CreatedAtRouteResult("GetProductById", new { id = newProductDTO.ProductId }, newProductDTO);
+        }
+        [HttpPatch("{id:int}/updatePartial")]
+        public ActionResult<ProductDTOUpdateResponse> UpdateProduct(int id, JsonPatchDocument<ProductDTOUpdateRequest> patchProductDTO)
+        {
+            if(patchProductDTO is null || id <= 0)
+            {
+                return BadRequest();
+            }
+            var product = _unityOfWork.ProductRepository.Get(p => p.ProductId == id);
+            if (product is null)
+            {
+                return NotFound("Produto não encontrado!");
+            }
+            var productUpdateRequest = _mapper.Map<ProductDTOUpdateRequest>(product);
+            patchProductDTO.ApplyTo(productUpdateRequest, ModelState);
+            if(!ModelState.IsValid || TryValidateModel(productUpdateRequest))
+            {
+                return BadRequest(ModelState);
+            }
+            _mapper.Map(productUpdateRequest, product);
+            _unityOfWork.Commit();
+            var updatedProductResponse = _mapper.Map<ProductDTOUpdateResponse>(product);
+            return Ok($"Produto atualizado!\nProduct: {updatedProductResponse}");
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<ProductDTO> EditProduct(int id, ProductDTO productDTO)
+        {
+            if(id != productDTO.ProductId) {
+                _logger.LogWarning("Dados inválidos");
+                return BadRequest("Dados inválidos");
+            }
+            var product = _mapper.Map<Product>(productDTO);
+            _unityOfWork.ProductRepository.Update(product);
+            _unityOfWork.Commit();
+            return Ok("Produto editado");
+        }
+        [HttpDelete("{id:int}")]
+        public ActionResult<ProductDTO> DeleteProduct(int id)
+        {
+            var product = _unityOfWork.ProductRepository.Get(p=> p.ProductId == id);
+            if(product is null)
+            {
+                _logger.LogWarning($"Produto com id {id} não encontrado");
+                return NotFound($"Produto com id {id} não encontrado");
+            }
+            _unityOfWork.ProductRepository.Delete(product);
+            _unityOfWork.Commit();
+            var productDTO = _mapper.Map<ProductDTO>(product);
+            return Ok($"Produto deletado\nProduct:{productDTO}");
+        }
+        private static object GetMetadata(PagedList<Product> products)
+        {
+            return new
+            {
+                products.PageSize,
+                products.TotalCount,
+                products.CurrentPage,
+                products.TotalPages,
+                products.hasNext,
+                products.hasPrevious
+            };
+        }
+    }
+}
